5cce5cb9ff1c4f9d68f15ff6b66ae9a5391a820a
Tanmay Sayare <144255204+Tanmay-say@users.noreply.github.com>
2024-01-13 13:41:55 +0530

    Mobile number is added in df

--- Random_Data_set.py
+++ Random_Data_set.py
@@ -5,13 +5,15 @@ import random
 dict = {
     'Name': [f'Student {i}' for i in range(1, 21)],
     'Age': [random.randint(18, 25) for _ in range(20)],
-    'Marks': [random.randint(60, 100) for _ in range(20)]
+    'Marks': [random.randint(60, 100) for _ in range(20)],
+    'M_Number': [random.randint(7000000000,9999999999) for _ in range(20)]
 }
 
 # Create a DataFrame
 df = pd.DataFrame(dict)
 
 # Save the DataFrame to a CSV file
-df.to_csv('students_data.csv', index=False)
+df.to_csv('students_data1.csv', index=False)
 
 
+print(df)
