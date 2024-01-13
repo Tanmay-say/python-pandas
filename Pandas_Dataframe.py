33c709e331d66cf562e1904bec4b4020de146b76
Tanmay Sayare <144255204+Tanmay-say@users.noreply.github.com>
2024-01-13 13:15:09 +0530

    Dictionary used pandas dataframes

---
+++ Pandas_Dataframe.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+
+dict = {
+    "Name " : ["Tanmay" ," mohit" , "sameer"],
+    "Age " : [18 ,20 ,70],
+    "city " : [" nagpur" , "bandara" , "shirdi"]
+}
+
+df = pd.DataFrame(dict)
+print(df)
